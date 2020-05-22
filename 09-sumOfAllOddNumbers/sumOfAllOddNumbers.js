24220756eb31837abbf2fd7e1e5f882a8f787341
Aswin Nair <aswintorch@gmail.com>
2020-05-22 08:30:23 -0400

    Solved problem 09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  // Returns the number of all odd numbers in input array
+
+  let count = 0;
+  nums.forEach((number) => {
+    // If number is odd, increase count
+    count += (number % 2 !== 0) ? 1 : 0;
+  });
+
+  return count;
 }
 
 // Do not edit this line;
