bb4fb0622707a81fd982005a4f752781d8663a31
Aswin Nair <aswintorch@gmail.com>
2020-05-22 08:49:54 -0400

    Solved problem 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,7 @@
 function isPalindrome(word) {
-  // Insert code here;
+  // Returns true/false if input string is/isn't a palindrome
+
+  return word === word.split('').reverse().join('');
 }
 
 // Do not edit this line;
