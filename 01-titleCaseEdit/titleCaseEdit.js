bfeafb62062d15124d7daf1f11645eb5345d94e7
Aswin Nair <aswintorch@gmail.com>
2020-05-21 20:07:36 -0400

    Solved problem 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  // The given title is first split into an array of words
+  let finalTitle =  title.split(' ')
+  // The map function iterates and uppercases each word in the array
+  .map((word) => {
+    return (word.charAt(0).toUpperCase() + word.slice(1))
+  }) // Finally, the join method is called to combine the words into one string
+  .join(' ')
+
+  return finalTitle;
 }
 
 // Do not edit this line;
