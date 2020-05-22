835c40c5c754512695b13804a0d3f457bd08e232
Aswin Nair <aswintorch@gmail.com>
2020-05-22 09:06:37 -0400

    Solved problem 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  // Outputs the frequency of letters in input word
+
+  let frequencyObj = {}
+
+  // Iterates through the word and maps the frequency of each letter in the object 
+  word.split('').forEach((letter) => {
+    frequencyObj[letter] = frequencyObj[letter] ? frequencyObj[letter] + 1: 1;
+  })
+
+  return frequencyObj;
 }
 
 // Do not edit this line;
