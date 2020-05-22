c772ef4a640183f9dd8a97133328e8e0b75ccf4f
Aswin Nair <aswintorch@gmail.com>
2020-05-22 08:34:27 -0400

    Solved problem 10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  // Returns the count of numbers in input array between the range of input start and end values
+
+  let count = 0;
+  nums.forEach((number) => {
+    count += (number >= start && number <= end) ? 1 : 0;
+  })
+
+  return count;
 }
 
 // Do not edit this line;
