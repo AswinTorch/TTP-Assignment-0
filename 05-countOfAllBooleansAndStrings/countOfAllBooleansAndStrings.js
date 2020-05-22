7f8a135d4b2799b89d1eda57ce0b00de00ab3039
Aswin Nair <aswintorch@gmail.com>
2020-05-21 20:28:19 -0400

    Solved problem 05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,16 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  // Returns the number of booleans and strings in input array
+
+  // Variable to track boolean/string count in array
+  let count = 0;
+  // Loops through array and increments count if a boolean or string is found
+  for (let a = 0; a < arr.length; a++){
+    if (arr[a] === true || arr[a] === false || typeof arr[a] === "string") {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 // Do not edit this line;
