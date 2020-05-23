776cecaa0ddec822031637dff509ac1f45805e52
Aswin Nair <aswintorch@gmail.com>
2020-05-23 12:21:17 -0400

    Added comments to problem 17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,24 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  // Returns a set with the union of all sets in the input args
+
+  let set = new Set();
+  let array = []
+
+  // Adds all the sets in the input args to an array
+  for (let a = 0; a < arguments.length; a++){
+    array.push(arguments[a])
+  }
+
+  // Loops through the array and each set element in the array and adds the values of the sets to a new set
+  for (let a = 0; a < array.length; a++){
+    for (var elem of array[a]) { 
+      set.add(elem); 
+    }
+  }
+  
+  // For testing
+  console.log(set)
+  return set;
 }
 
 // Do not edit this line;
