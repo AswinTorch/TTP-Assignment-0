ccff934ae8d191aeeefe8bbf834d5f897a9fdde3
Aswin Nair <aswintorch@gmail.com>
2020-05-23 23:02:07 -0400

    Solved problem 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,20 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  // Checks if two distinct nums in the input array sum to input target number
+
+  // Error check
+  if (nums.length <= 1) throw "Length of array is not more than 1!";
+  
+  // Loops through each element of the array and after validating, checks if sums to target num
+  for (let a = 0; a < nums.length; a++) {
+    for (let b = 0; b < nums.length; b++){
+      if (a !== b && nums[a] + nums[b] === target) {
+        return true;
+      }
+    }
+  }
+
+  // If nothing found, return false
+  return false;
 }
 
 // Do not edit this line;
